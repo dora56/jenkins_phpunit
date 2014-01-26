03baf6a8ee8298308a37c53239cb908adcdb7de9
YUTA YAMAMOTO <tuki0918@gmail.com>
2014-01-26 23:23:16 +0900

    first commit.

---
+++ tests/sample1Test.php
@@ -0,0 +1,35 @@
+<?php
+class Sample1Test extends PHPUnit_Framework_TestCase
+{
+	public function testPushAndPop()
+	{
+		$user = array();
+		$this->assertEquals(0, count($user));
+
+		array_push($user, 'hoge');
+		$this->assertEquals('hoge', $user[0]);
+		$this->assertEquals(1, count($user));
+
+		$this->assertEquals('hoge', array_pop($user));
+		$this->assertEquals(0, count($user));
+	}
+
+	public function testArray1()
+	{
+		$array = array();
+		$this->assertEquals(0, count($array));
+	}
+
+	public function testArray2()
+	{
+		$array = array();
+		$this->assertEquals(0, count($array));
+	}
+
+	public function testArray3()
+	{
+		$array = array(1, 2, 3);
+		$this->assertEquals(3, count($array));
+	}
+
+}
